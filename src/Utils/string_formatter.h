4629a630e40d21412f8c5d2fb1ee2a3af8acc003
sxf <sun_xfancy@163.com>
2016-02-16 20:48:33 +0800

    修改了一个std的声明

--- src/Utils/string_formatter.h
+++ src/Utils/string_formatter.h
@@ -6,16 +6,13 @@
 #include <iostream>
 #include <string>
 
-
-using namespace std; //Don't if you're in a header-file
-
 template<typename ... Args>
-string string_format( const std::string& format, Args ... args )
+std::string string_format( const std::string& format, Args ... args )
 {
     size_t size = snprintf( nullptr, 0, format.c_str(), args ... ) + 1; // Extra space for '\0'
-    unique_ptr<char[]> buf( new char[ size ] );
+    std::unique_ptr<char[]> buf( new char[ size ] );
     snprintf( buf.get(), size, format.c_str(), args ... );
-    return string( buf.get(), buf.get() + size - 1 ); // We don't want the '\0' inside
+    return std::string( buf.get(), buf.get() + size - 1 ); // We don't want the '\0' inside
 }
 
 
